134daae322c359894943360b36ec71008e273e74
almuheetushihab <almuheetu@gmail.com>
2024-10-10 12:41:23 +0600

    try to text show in screen using compose

--- app/src/main/java/com/example/declarativeui/MainActivity.kt
+++ app/src/main/java/com/example/declarativeui/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.declarativeui
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.declarativeui.ui.theme.DeclarativeUiTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +27,9 @@ class MainActivity : ComponentActivity() {
             DeclarativeUiTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android studio",
+                        age = 20,
+                        list = listOf("a", "b", "c"),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +39,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String, age: Int, list: List <String>,image: Image, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
+        modifier = Modifier.padding(24.dp)
+    )
+    Text(
+        text = "I am $age years old",
         modifier = modifier
     )
+    list.forEach { _ ->
+        Text(
+            text = "my list is $list",
+            modifier = modifier.padding(54.dp).padding(12.dp).padding(34.dp).padding(24.dp).then(modifier.border(2.dp, androidx.compose.ui.graphics.Color.Black)).then(modifier.background(color = androidx.compose.ui.graphics.Color.Green))
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DeclarativeUiTheme {
-        Greeting("Android")
+        Greeting("Android studio", 20, listOf("a", "b", "c"))
     }
 }
