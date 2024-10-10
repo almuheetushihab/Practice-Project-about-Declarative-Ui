206ef3f191fbdc31dd14bf7656042d5accc9da38
almuheetushihab <almuheetu@gmail.com>
2024-10-10 18:11:41 +0600

    forget button add

--- app/src/main/java/com/example/declarativeui/LoginScreen.kt
+++ app/src/main/java/com/example/declarativeui/LoginScreen.kt
@@ -1,25 +1,19 @@
 package com.example.declarativeui
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -31,7 +25,11 @@ import androidx.compose.ui.unit.sp
 fun LoginScreen() {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxWidth().fillMaxHeight()
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(16.dp)
+            .padding(top = 80.dp)
     ) {
         Text(
             text = "Welcome, User",
@@ -54,7 +52,7 @@ fun LoginScreen() {
         TextField(
             value = "",
             onValueChange = {},
-            placeholder = { Text(text = "Fullname") },
+            placeholder = { Text(text = "FullName") },
             modifier = Modifier
                 .height(100.dp)
                 .padding(top = 24.dp, start = 16.dp, end = 16.dp)
@@ -81,16 +79,25 @@ fun LoginScreen() {
                 .fillMaxWidth(),
             singleLine = true
         )
+        Text(
+            text = "Forgot Password?",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Blue,
+            modifier = Modifier
+                .align(Alignment.End)
+                .padding(end = 16.dp, top = 8.dp),
+        )
         Button(
             onClick = {},
             modifier = Modifier
                 .width(200.dp)
                 .height(100.dp)
-                .padding(top = 50.dp),
-
+                .padding(top = 40.dp)
         ) {
             Text(text = "Login")
         }
+
     }
 }
 
