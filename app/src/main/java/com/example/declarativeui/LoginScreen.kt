b7c658d3a5bfda619f92c23c80978dab7b3e0463
almuheetushihab <almuheetu@gmail.com>
2024-10-10 17:36:51 +0600

    login Screen design in declarative  Ui

--- app/src/main/java/com/example/declarativeui/LoginScreen.kt
+++ app/src/main/java/com/example/declarativeui/LoginScreen.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -17,76 +19,83 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun LoginScreen(){
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White)
-            .padding(16.dp),
-        contentAlignment = Alignment.TopStart
+fun LoginScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth().fillMaxHeight()
+    ) {
+        Text(
+            text = "Welcome, User",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            modifier = Modifier
+                .padding(16.dp)
+                .height(30.dp)
+                .align(Alignment.CenterHorizontally)
 
-    ){
-       Column(
-           horizontalAlignment = Alignment.CenterHorizontally,
-           modifier = Modifier.verticalScroll(rememberScrollState())
-       ){
-           Text(
-               text = "Welcome to the Login Screen",
-               color = Color.Black,
-               modifier = Modifier
-                   .padding(16.dp)
-                   .height(25.dp)
-                   .align(Alignment.CenterHorizontally)
-           )
-           Image(
-               painter = painterResource(id = R.drawable.product_1),
-               contentDescription = "Product Image",
-               modifier = Modifier
-                   .size(150.dp)
-                   .padding(top = 16.dp)
-           )
-           TextField(
-               value = "",
-               onValueChange = {},
-               placeholder = { Text(text = "Username")},
-               modifier = Modifier
-                   .fillMaxSize()
-                   .height(100.dp)
-                   .padding(top = 40.dp),
-               singleLine = true
-           )
-           TextField(
-               value = "",
-               onValueChange = {},
-               placeholder = { Text(text = "Password")},
-               modifier = Modifier
-                   .fillMaxSize()
-                   .height(100.dp)
-                   .padding(top = 40.dp),
-               singleLine = true
-           )
-           Button(
-               onClick = {},
-               modifier = Modifier
-                   .width(200.dp)
-                   .height(100.dp)
-                   .padding(top = 40.dp)
-           ) {
-               Text(text = "Login")
-           }
-       }
+        )
+        Image(
+            painter = painterResource(id = R.drawable.product_1),
+            contentDescription = "Product Image",
+            modifier = Modifier
+                .size(150.dp)
+                .padding(top = 16.dp, bottom = 16.dp)
+        )
+        TextField(
+            value = "",
+            onValueChange = {},
+            placeholder = { Text(text = "Fullname") },
+            modifier = Modifier
+                .height(100.dp)
+                .padding(top = 24.dp, start = 16.dp, end = 16.dp)
+                .fillMaxWidth(),
+            singleLine = true
+        )
+        TextField(
+            value = "",
+            onValueChange = {},
+            placeholder = { Text(text = "Username") },
+            modifier = Modifier
+                .height(100.dp)
+                .padding(top = 24.dp, start = 16.dp, end = 16.dp)
+                .fillMaxWidth(),
+            singleLine = true
+        )
+        TextField(
+            value = "",
+            onValueChange = {},
+            placeholder = { Text(text = "Password") },
+            modifier = Modifier
+                .height(100.dp)
+                .padding(top = 24.dp, start = 16.dp, end = 16.dp)
+                .fillMaxWidth(),
+            singleLine = true
+        )
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .width(200.dp)
+                .height(100.dp)
+                .padding(top = 50.dp),
 
+        ) {
+            Text(text = "Login")
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun LoginScreenPreview(){
+fun LoginScreenPreview() {
     LoginScreen()
 }
